e75f6315c33c59ba481cc10ed59e8fad9ebd40df
Praneeth Jadcherla <praneethjadcherla96@gmail.com>
2023-02-26 14:10:46 +0000

    Created and passed testcase checkNoSeatIsAlllocated

--- src/test/java/com/techreturners/MovieTheatreTest.java
+++ src/test/java/com/techreturners/MovieTheatreTest.java
@@ -7,9 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MovieTheatreTest {
       @Test
-      public void checkEmptyTheatre(){
+      public void checkEAllSeatsAvailable(){
             MovieTheatre movieTheatre=new MovieTheatre();
 
             assertEquals(15,movieTheatre.createAllSeatsAvailable());
       }
+
+      @Test
+      public void checkNoSeatIsAllocated(){
+            MovieTheatre movieTheatre=new MovieTheatre();
+
+            assertEquals(0,movieTheatre.allocatedSeats.size());
+      }
+
 }
